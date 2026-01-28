ee66524025e6d5a9e51f9fc8a773a453550e8c6f
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2026-01-28 15:52:14 -0800

    add session

--- preprocess.sh
+++ preprocess.sh
@@ -4,8 +4,8 @@
 path_script=~/codes/testing-sherlock
 PATH_DATA=${SCRATCH}/k23_data/derivatives
 output_path=${SCRATCH}/k23_data/preprocessing_2026-01-28
-subjects=(sub-CR012/ses-baselinespinalcord)
-
+subjects=(sub-CR012)
+ses=ses-baselinespinalcord
 time_limit=02:00:00
 memory=16000
 
@@ -21,8 +21,8 @@ mkdir -p ${PATH_QC}
 
 for subject in "${subjects[@]}"; do
     echo "Preprocessing data for subject: $subject"
-    export subject path_script PATH_DATA SCRATCH output_path time_limit memory PATH_DATA PATH_DATA_PROCESSED PATH_RESULTS PATH_LOG PATH_QC
-    envsubst '${subject} ${path_script} ${PATH_DATA} ${SCRATCH} ${output_path} ${time_limit} ${memory} ${PATH_DATA} ${PATH_DATA_PROCESSED} ${PATH_RESULTS} ${PATH_LOG} ${PATH_QC}' < ${path_script}/preprocess.sbatch > preprocess_${subject}.sbatch
+    export subject path_script PATH_DATA SCRATCH output_path time_limit memory PATH_DATA PATH_DATA_PROCESSED PATH_RESULTS PATH_LOG PATH_QC ses
+    envsubst '${subject} ${path_script} ${PATH_DATA} ${SCRATCH} ${output_path} ${time_limit} ${memory} ${PATH_DATA} ${PATH_DATA_PROCESSED} ${PATH_RESULTS} ${PATH_LOG} ${PATH_QC} ${ses}' < ${path_script}/preprocess.sbatch > preprocess_${subject}.sbatch
     sbatch preprocess_${subject}.sbatch
     rm preprocess_${subject}.sbatch
     sleep 10s
